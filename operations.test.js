d4e84547d0abcefac361739f507417a315cfb2f4
karim <karimjoudi01@gmail.com>
2022-07-26 09:02:59 +0100

    testing operations

--- operations.test.js
+++ operations.test.js
@@ -1,13 +1,17 @@
-const { add, soustraction, multiplication } = require("./operations");
+const { add, soustraction, multiplication, division } = require("./operations");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test("soustracts 10 / 2 to equal 10", () => {
-  expect(soustraction(10, 2)).toBe(5);
+test("soustracts 10 - 2 to equal 8", () => {
+  expect(soustraction(10, 2)).toBe(8);
 });
 
 test("multiplies 5 * 2 to equal 10", () => {
   expect(multiplication(5, 2)).toBe(10);
+});
+
+test("divides 10 / 2 to equal 5", () => {
+  expect(division(10, 2)).toBe(5);
 });
