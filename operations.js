620a4f7688b1080f1e9a63dba89a27df0ebc0821
karim <karimjoudi01@gmail.com>
2022-07-26 09:00:43 +0100

    first commit

--- operations.js
+++ operations.js
@@ -2,10 +2,13 @@ const add = (a, b) => {
   return a + b;
 };
 const soustraction = (a, b) => {
-  return a / b;
+  return a - b;
 };
 const multiplication = (a, b) => {
   return a * b;
 };
+const division = (a, b) => {
+  return a / b;
+};
 
-module.exports = { add, soustraction, multiplication };
+module.exports = { add, soustraction, multiplication, division };
