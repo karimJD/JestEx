8cbbf1c886accc7fe2cfd2f222ae9e35b83d2e34
karim <karimjoudi01@gmail.com>
2022-07-26 10:34:56 +0100

    removed division by 0 test

--- tests/division.test.js
+++ tests/division.test.js
@@ -7,7 +7,3 @@ test("divides 10 / 2 to equal 5", () => {
 test("divides -10 / 2 to equal -5", () => {
   expect(division(-10, 2)).toBe(-5);
 });
-
-test("divides 10 / 0 to equal undefined", () => {
-  expect(division(10, 0)).toBe(Infinity);
-});
